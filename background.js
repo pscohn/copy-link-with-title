0afa21480fbaf1e4825b1b4b5a16bb9b8477fa07
Paul Cohn <pscohn@gmail.com>
2017-05-12 23:34:25 -0400

    initial commit

---
+++ background.js
@@ -0,0 +1,33 @@
+function copyTextToClipboard(text) {
+  var input = document.createElement('textarea');
+  document.body.appendChild(input);
+  input.value = text;
+  input.focus();
+  input.select();
+  document.execCommand('Copy');
+  input.remove();
+}
+
+function getFormattedLink(title, url, callback) {
+  chrome.storage.sync.get(['format', 'custom'], function(data) {
+    var formattedLink;
+    if (data.format == 'md') {
+      formattedLink = `[${title}](${url})`;
+    } else if (data.format == 'html') {
+      formattedLink = `<a href="${url}">${title}</a>`;
+    } else if (data.format == 'basic') {
+      formattedLink = `${title} - ${url}`;
+    } else if (data.format == 'custom') {
+      formattedLink = data.custom.replace(/\[title\]/g, title).replace(/\[url\]/g, url);
+    }
+    callback(formattedLink);
+  });
+}
+
+chrome.browserAction.onClicked.addListener(
+  function(tab) {
+    getFormattedLink(tab.title, tab.url, function(formattedLink) {
+      copyTextToClipboard(formattedLink);
+    });
+  }
+);
---
+++ options.js
@@ -0,0 +1,26 @@
+var validOptions = ['md', 'custom', 'basic', 'html'];
+
+function saveOptions(event) {
+  if (validOptions.indexOf(event.target.value) > -1) {
+    chrome.storage.sync.set({ format: event.target.value });
+  }
+}
+
+function saveCustomText(event) {
+  chrome.storage.sync.set({ custom: event.target.value });
+}
+
+function restoreOptions() {
+  chrome.storage.sync.get(['format', 'custom'], function(data) {
+    document.getElementById('custom-text').value = data.custom;
+    var radio = document.getElementById(data.format)
+    radio.checked = true;
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  restoreOptions();
+  document.querySelector('form').onchange = saveOptions;
+  document.getElementById('custom-text').onchange = saveCustomText;
+});
+
