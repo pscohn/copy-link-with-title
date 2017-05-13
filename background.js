7b1ca606fbec4d415ff571f4413d3d7836beb857
Paul Cohn <pscohn@gmail.com>
2017-05-13 00:18:45 -0400

    fix default case

--- background.js
+++ background.js
@@ -19,6 +19,8 @@ function getFormattedLink(title, url, callback) {
       formattedLink = `${title} - ${url}`;
     } else if (data.format == 'custom') {
       formattedLink = data.custom.replace(/\[title\]/g, title).replace(/\[url\]/g, url);
+    } else {
+      formattedLink = `${title} - ${url}`;
     }
     callback(formattedLink);
   });
